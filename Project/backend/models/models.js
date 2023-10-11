83fe2c9a3b6cb676f26c4f00420f4a356a34a840
MTStokes2 <mtstoke2@cougarnet.uh.edu>
2023-10-11 17:18:13 -0500

    Customers, Admins, Usernames, Passwords, City, State models

--- Project/backend/models/models.js
+++ Project/backend/models/models.js
@@ -7,10 +7,12 @@ const config = require('../src/config/config')
 //Database
 const database = config.database
 
-//Test Model used as an example
+//---------------------------------------------------//
+//             Test Model                            //
+//---------------------------------------------------//
 const Test_Model = database.define('TEST', {
     idTEST: {
-        type: Sequelize.CHAR,
+        type: Sequelize.INTEGER,
         primaryKey: true
     },
     TESTcol: {
@@ -21,6 +23,175 @@ const Test_Model = database.define('TEST', {
     },
     TESTcol2: {
         type: Sequelize.CHAR
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//             Customers Model                        //
+//---------------------------------------------------//
+const Customers_Model = database.define('CUSTOMERS', {
+    CustomerID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    UsernameID: {
+        type: Sequelize.INTEGER
+    },
+    PasswordID: {
+        type: Sequelize.INTEGER
+    },
+    CityID: {
+        type: Sequelize.INTEGER
+    },
+    StateID: {
+        type: Sequelize.INTEGER
+    },
+    ZipCode: {
+        type: Sequelize.NUMBER
+    },
+    CustomerLastName: {
+        type: Sequelize.STRING
+    },
+    CustomerFirstName: {
+        type: Sequelize.STRING
+    },
+    CustomerAddress: {
+        type: Sequelize.STRING
+    },
+    CustomerPhone: {
+        type: Sequelize.STRING
+    },
+    CustomerEmail: {
+        type: Sequelize.STRING
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//               Admins Model                        //
+//---------------------------------------------------//
+const Admins_Model = database.define('ADMINS', {
+    AdminID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    UsernameID: {
+        type: Sequelize.INTEGER
+    },
+    PasswordID: {
+        type: Sequelize.INTEGER
+    },
+    AdminLastName: {
+        type: Sequelize.STRING
+    },
+    AdminFirstName: {
+        type: Sequelize.STRING
+    },
+    AdminAddress: {
+        type: Sequelize.STRING
+    },
+    AdminPhone: {
+        type: Sequelize.STRING
+    },
+    AdminEmail: {
+        type: Sequelize.STRING
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//             Usernames Model                       //
+//---------------------------------------------------//
+const Usernames_Model = database.define('USERNAMES', {
+    UsernameID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    CustomerID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    AdminID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    Username: {
+        type: Sequelize.STRING
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//             Passwords Model                       //
+//---------------------------------------------------//
+const Passwords_Model = database.define('PASSWORDS', {
+    PasswordID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    CustomerID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    AdminID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    Password: {
+        type: Sequelize.STRING
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//                 City Model                        //
+//---------------------------------------------------//
+const City_Model = database.define('CITY', {
+    CityID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    City: {
+        type: Sequelize.STRING
+    }},
+     {
+        freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
+        createdAt: false, //Removes the createdAt field from the query since we don't have it in our tables
+        updatedAt: false //Removes the updatedAt field from the query since we don't have it in our tables
+    }
+);
+
+//---------------------------------------------------//
+//                State Model                        //
+//---------------------------------------------------//
+const State_Model = database.define('STATE', {
+    StateID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    State: {
+        type: Sequelize.STRING
     }},
      {
         freezeTableName: true, //makes sure the sql script uses the defined table name 'TEST' instead of TESTs
